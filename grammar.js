d40256438140c75e8476e1e07c6c58f2875ff0b6
Robin Putters <robin.putters@keronic.com>
2023-02-12 16:32:42 +0100

    Add comments, method documentation, some expressions

--- grammar.js
+++ grammar.js
@@ -1,117 +1,153 @@
 module.exports = grammar({
-    name: 'magik',
+    name: "magik",
 
     rules: {
-	source_file: $ =>
-	    repeat(
-		choice(
-		    $._definition,
-		    $._literal,
-		    $.block
-		),
-	    ),
+	source_file: $ => repeat($.fragment),
 
-	_definition: $ =>
-	    choice(
-		$.method_declaration
-	    ),
+	fragment: $ =>
+	prec.left(repeat1(seq($._statement, optional(seq("\n$\n"))))),
 
 	method_declaration: $ =>
+	prec.left(
 	    seq(
-		'_method',
-		field('exemplarname', $.identifier),
-		'.',
-		field('name', $.identifier),
+		optional($.pragma),
+		"_method",
+		field("exemplarname", $.identifier),
+		".",
+		field("name", $.identifier),
 		optional($.parameter_list),
+		optional($.documentation),
 		repeat($._statement),
-		'_endmethod',
-		optional('\n$\n')
-	    ),
+		"_endmethod"
+	    )
+	),
 
 	parameter_list: $ =>
+	prec.left(
 	    choice(
-		seq(
-		    '(',
-		    optional(
-			seq(
-			    commaSep1($.parameter),
-			    optional(',')
-			)
-		    ),
-		    ')'
-		),
-		seq(
-		    '<<',
-		    seq(
-			commaSep1($.parameter),
-			optional(',')
-		    )
-		)
-	    ),
+		seq("(", optional(seq(commaSep1($.parameter), optional(","))), ")"),
+		seq("<<", seq(commaSep1($.parameter), optional(",")))
+	    )
+	),
 
 	parameter: $ => $.identifier,
 
 	block: $ =>
-	    seq(
-		'_block',
-		repeat($._statement),
-		'_endblock',
-		optional('\n$\n')
-	    ),
+	prec.left(
+	    seq("_block", repeat($._statement), "_endblock", optional("\n$\n"))
+	),
+
+	if_expression: $ =>
+	seq(
+	    "_if",
+	    field("condition", $._expression),
+	    "_then",
+	    repeat($._statement),
+	    repeat(seq("_elif", repeat($._statement))),
+	    optional(seq("_else", repeat($._statement))),
+	    "_endif"
+	),
+
+	pragma: $ => seq("_pragma(", /.*/, ")"),
 
 	_literal: $ =>
-	    choice(
-		$.true,
-		$.false,
-		$.maybe,
-		//	    $.character_literal,
-		$.string_literal,
-		$.number,
-		$.unset
-	    ),
+	choice(
+	    $.true,
+	    $.false,
+	    $.maybe,
+	    //	    $.character_literal,
+	    $.string_literal,
+	    $.number,
+	    $.unset,
+	    $.super,
+	    $.self
+	),
 
 	string_literal: $ =>
-	    token(
-		choice(
-		    seq('"', repeat(choice(/[^\\"\n]/, /\\(.|\n)/)), '"'),
-		    seq('\'', repeat(choice(/[^\\"\n]/, /\\(.|\n)/)), '\'')
-		)
-	    ),
+	token(
+	    choice(
+		seq('"', repeat(choice(/[^\\"\n]/, /\\(.|\n)/)), '"'),
+		seq("'", repeat(choice(/[^\\"\n]/, /\\(.|\n)/)), "'")
+	    )
+	),
 
-	true: $ => '_true',
-	false: $ => '_false',
-	maybe: $ => '_maybe',
-	unset: $ => '_unset',
+	call: $ =>
+	prec.left(
+	    seq(
+		field("receiver", $.identifier),
+		field("operator", "."),
+		field("message", $.identifier),
+		optional($.parameter_list)
+	    )
+	),
+
+	true: $ => "_true",
+	false: $ => "_false",
+	maybe: $ => "_maybe",
+	unset: $ => "_unset",
+	super: $ => "_super",
+	self: $ => "_self",
 
 	_statement: $ =>
-	    choice(
-		$.return_statement
-		// TODO: other kinds of statements
-	    ),
+	choice(
+	    $.method_declaration,
+	    $.block,
+	    $.comment,
+	    $.return_statement,
+	    $._expression
+	    // TODO: other kinds of statements
+	),
 
 	return_statement: $ =>
+	prec.left(
 	    choice(
-		seq('_return', optional($._expression)),
-		seq('>>', $._expression)
-	    ),
+		seq("_return", optional($._expression)),
+		seq(">>", $._expression)
+	    )
+	),
 
 	_expression: $ =>
-	    choice(
-		$.identifier,
-		$._literal
-		// TODO: other kinds of expressions
-	    ),
+	choice(
+	    $.call,
+	    $.if_expression,
+	    $.logical_operator,
+	    $.relational_operator,
+	    $.identifier,
+	    $._literal
+	    // TODO: other kinds of expressions
+	),
 
-	identifier: $ => /[a-z_][a-z0-9_]*/,
+	identifier: $ => /[a-z][a-z0-9_\?]*/,
 
-	number: $ => /\d+/
-    }
-})
+	number: $ => /\d+/,
+
+	relational_operator: $ =>
+	prec.right(
+	    seq(
+		field("left", $._expression),
+		field("operator", choice("_is", "_isnt", "=", "~=", "<>", ">=", "<=")),
+		field("right", $._expression)
+	    )
+	),
+
+	logical_operator: $ =>
+	prec.left(
+	    seq(
+		field("left", $._expression),
+		field("operator", choice("_and", "_or", "_xor", "_andif", "_orif", "_xorif")),
+		field("right", $._expression)
+	    )
+	),
+
+	documentation: $ => repeat1(/##.*/),
+	comment: $ => prec.right(repeat1(/#.*/)),
+    },
+});
 
 function commaSep1(rule) {
-    return sep1(rule, ',')
+    return sep1(rule, ",");
 }
 
 function sep1(rule, separator) {
-    return seq(rule, repeat(seq(separator, rule)))
+    return seq(rule, repeat(seq(separator, rule)));
 }
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -22,12 +22,38 @@
   (unset)
 ] @constant.builtin
 
+[
+ (self)
+ (super)
+] @variable.builtin
+
+(documentation) @attribute
+(comment) @comment
+(pragma) @attribute
+
+; Expression
+
+(call
+  receiver: (identifier) @variable.parameter)
+(call 
+  operator: "." @operator)
+(call
+  method: (identifier) @function)
+
 ; Keywords
 
 [
   "_method"
   "_endmethod"
+
   "_block"
   "_endblock"
+
+  "_if"
+  "_then"
+  "_elif"
+  "_else"
+  "_endif"
+  
   "_return"
 ] @keyword
